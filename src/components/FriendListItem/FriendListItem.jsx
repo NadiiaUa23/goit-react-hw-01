2f5cf738bed17513b6a68297c67c00d3c9f56fcf
NadiiaUa23 <hasiminka92@gmail.com>
2024-07-01 12:47:29 +0200

    компонент FriendListItem

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,2 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from '../FriendListItem/FriendListItem.module.css';
 
-const FriendList = (friends ) => 
+const FriendListItem = ({ avatar, name, isOnline }) => (
+    <li className={styles.item}>
+      <img className={styles.avatar} src={avatar} alt={`${name}'s avatar`} width="48" />
+      <p className={styles.name}>{name}</p>
+      <p className={isOnline ? styles.online : styles.offline}>{isOnline ? 'Online' : 'Offline'}</p>
+    </li>
+  );
+  
+  FriendListItem.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+  };
+  
+  export default FriendListItem;
