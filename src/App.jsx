911c0c34e5f56c708bdb8c4207ef96afae92959a
NadiiaUa23 <hasiminka92@gmail.com>
2024-07-01 20:06:31 +0200

    add fix task

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import Profile from './components/Profile/Profile';
 import userData from './userData.json';
+import friends from "../friends.json";
+import FriendList from './components/FriendList/FriendList';
 
 const App = () => {
   return (
-  <div>
-    <Profile
-      name={userData.username}
-      tag={userData.tag}
-      location={userData.location}
-      image={userData.avatar}
-      stats={userData.stats}
-    />
-  </div>
-);
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+    </>
+  );
 };
+
 export default App;
